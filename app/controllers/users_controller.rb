4f4086c3da039c7c201452d236698ae43ae878c9
Ide Kazuki <c1000919@planet.kanazawa-it.ac.jp>
2020-09-29 18:46:45 +0900

    9/29現在の状況

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       redirect_to @user
     else
+      flash[:alert] = "エラーが発生しました。#{@user.errors.full_messages}"
       render 'new'
     end
+  end
+  private
+
+  def index
+    params.require(:user).permit(:name, :email)
+  end
+  def user_list
+    @user = User.all
+  end
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_url
   end
 end
